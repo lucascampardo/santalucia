ee84ccde036577adaeaa16534bf01fa42729dee6
Lucas <74873871+lucascampardo@users.noreply.github.com>
2023-05-11 22:30:03 -0300

    Exercício de Fixação

---
+++ ex024.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+// Programa de Ordenação
+main() {
+    int x,vetor[]={14,2,1,9,40};
+    printf("Vetor desordenado \n");
+    for(x=0;x<5;x++)
+        printf("[%d]=%d \n",x,vetor[x]);
+}
